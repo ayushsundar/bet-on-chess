1268f7fa43a469491649ca877193c902a3fe507b
ayushsundar <ayushs@umd.edu>
2025-05-26 07:31:47 +0200

    Implemented odds engine to blend Stockfish & Magnus move frequencies

--- src/board_state.py
+++ src/board_state.py
@@ -1,12 +1,13 @@
 import chess # type: ignore
 
+# Applies a list of moves onto a new board and return the current board state
 def get_board_after_moves(moves):
-    # Applies a list of moves onto a new board and return the current board state
     board = chess.Board()
     for move in moves:
         board.push_san(move)
     return board
 
+# Calculating some very basic features to make sure I'm able to access informaiton from off of a PGN file chess game
 def get_basic_features(board):
     white_material = sum([len(board.pieces(piece_type, chess.WHITE)) for piece_type in range(1,7)])
     black_material = sum([len(board.pieces(piece_type, chess.BLACK)) for piece_type in range(1,7)])
--- src/odds_engine.py
+++ src/odds_engine.py
@@ -1,10 +1,52 @@
 import chess.engine
+import numpy as np
+import pickle
 
-engine = chess.engine.SimpleEngine.popen_uci("/Users/ayush/Downloads/stockfish/stockfish-macos-m1-apple-silicon")
-board = chess.Board()
-result = engine.analyse(board, chess.engine.Limit(depth=15))
-keys_list = list(result.keys())
-print(keys_list)
-print(result['pv'])
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
 
-engine.quit()
+def get_top_moves_with_odds(board, stockfish_path, magnus_model_path, top_n=3, depth=15):
+    
+    # Connecting to Stockfish
+    engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
+
+    # Getting the top Stockfish moves
+    info = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=top_n)
+    stockfish_moves = []
+    stockfish_scores = []
+
+    for entry in info:
+        move = entry["pv"][0]
+        score = entry["score"].relative.score(mate_score=10000) # Centipawn score
+        
+        if score is None:
+            score = 0
+        stockfish_moves.append(board.san(move))
+        stockfish_scores.append(score)
+    # Converting the Stockfish scores to probabilities
+    stockfish_probs = softmax(np.array(stockfish_scores))
+    
+    # Loading Magnus move freq
+    with open(magnus_model_path, "rb") as f:
+        magnus_move_probs_dict = pickle.load(f)
+    fen = board.fen()
+    magnus_probs = magnus_move_probs_dict.get(fen, {})
+
+    # Creating blended probabilites using Magnus move freq & stockfish
+    final_probs = {}
+
+    for i, move in enumerate(stockfish_moves):
+        stockfish_prob = stockfish_probs[i]
+        magnus_prob = magnus_probs.get(move, 1e-6) # Super small probability if move not seen before
+
+        # 70% Magnus 30% Stockfish
+        final_probs[move] = 0.7 * magnus_prob + 0.3 * stockfish_prob
+
+    # Normalizing final probabilities
+    total = sum(final_probs.values())
+    for move in final_probs:
+        final_probs[move] /= total
+    
+    engine.quit()
+    return final_probs
